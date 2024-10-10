9ce16ac05e1689f88a66bf5bdc1183ecb3eb5ad3
theajack <1506282385@qq.com>
2024-10-10 23:57:35 +0800

    feat: vite-vue3

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Author: theajack
+ * @Date: 2023-04-04 23:20:27
+ * @Description: Coding something
+-->
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <router-link to="/">home</router-link>
+    |
+    <router-link to="/about">about</router-link>
+  </div>
+  <router-view/>
+  <!-- <router-view v-slot="{ Component }">
+      <component :is="Component" />
+  </router-view> -->
+</template>
+<style lang="less">
+</style>
